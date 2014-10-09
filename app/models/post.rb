b52a47458efcedab5651d95ea5d57fd28fb7f94a
eliam2 <eliam.medina@gmail.com>
2014-10-09 21:26:41 +0000

    Added voting functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,38 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+  #validates :topic, presence: true
+  #validates :user, presence: true
+
+  after_create :create_vote
 
   mount_uploader :image, ImageUploader
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
+  def update_rank
+    age = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) #1 day in seconds
+    new_rank = points + age
+
+    update_attribute(:rank, new_rank)
+  end
 
   def markdown_title
     render_as_markdown title
@@ -27,6 +49,10 @@ class Post < ActiveRecord::Base
     extensions = {fenced_code_blocks: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
+  end
+
+  def create_vote
+    user.votes.create(post: self, value: 1)
   end
 
 end
